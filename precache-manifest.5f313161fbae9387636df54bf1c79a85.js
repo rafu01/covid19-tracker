d8525846832c046d3b2eab3718187c38d3b20652
rafu01 <nrafsan13@gmail.com>
2020-05-28 23:13:55 +0600

    Updates

---
+++ precache-manifest.5f313161fbae9387636df54bf1c79a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d16eef5f7950d1c0ba47d2caa2d10a",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "8deb69893726b0cac176",
+    "url": "/covid19-tracker/static/css/main.be03b3da.chunk.css"
+  },
+  {
+    "revision": "b60b8074820c5599101b",
+    "url": "/covid19-tracker/static/js/2.2cce2bed.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.2cce2bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8deb69893726b0cac176",
+    "url": "/covid19-tracker/static/js/main.ce511b10.chunk.js"
+  },
+  {
+    "revision": "cebe6393b809a16329cc",
+    "url": "/covid19-tracker/static/js/runtime-main.bbd307d8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
